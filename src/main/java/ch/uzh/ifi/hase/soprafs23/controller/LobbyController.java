4eb83c3ea6821560265122f03d117ae0398488c5
Suleman <sulekhan111@gmail.com>
2023-03-31 14:45:40 +0200

    Created LobbyController

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/controller/LobbyController.java
@@ -0,0 +1,103 @@
+package ch.uzh.ifi.hase.soprafs23.controller;
+
+import ch.uzh.ifi.hase.soprafs23.entity.User;
+import ch.uzh.ifi.hase.soprafs23.rest.dto.UserGetDTO;
+import ch.uzh.ifi.hase.soprafs23.rest.dto.UserPostDTO;
+import ch.uzh.ifi.hase.soprafs23.rest.dto.UserPutDTO;
+import ch.uzh.ifi.hase.soprafs23.rest.mapper.DTOMapper;
+import ch.uzh.ifi.hase.soprafs23.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User Controller
+ * This class is responsible for handling all REST request that are related to
+ * the user.
+ * The controller will receive the request and delegate the execution to the
+ * UserService and finally return the result.
+ */
+@RestController
+@RequestMapping("/users")
+public class LobbyController {
+
+  private final UserService userService;
+
+  LobbyController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @GetMapping("")
+  @ResponseStatus(HttpStatus.OK)
+  @ResponseBody
+  public List<UserGetDTO> getAllUsers() {
+    // fetch all users in the internal representation
+    List<User> users = userService.getUsers();
+    List<UserGetDTO> userGetDTOs = new ArrayList<>();
+
+    // convert each user to the API representation
+    for (User user : users) {
+      userGetDTOs.add(DTOMapper.INSTANCE.convertEntityToUserGetDTO(user));
+    }
+    return userGetDTOs;
+  }
+
+  //registration
+  @PostMapping("/register")
+  @ResponseStatus(HttpStatus.CREATED)
+  @ResponseBody
+  public UserGetDTO createUser(@RequestBody UserPostDTO userPostDTO) {
+    // convert API user to internal representation
+    User userInput = DTOMapper.INSTANCE.convertUserPostDTOtoEntity(userPostDTO);
+
+    // create user
+    User createdUser = userService.createUser(userInput);
+    // convert internal representation of user back to API
+    return DTOMapper.INSTANCE.convertEntityToUserGetDTO(createdUser);
+  }
+
+  @PostMapping("/login")
+  @ResponseStatus(HttpStatus.OK)
+  @ResponseBody
+  public UserGetDTO loginUser(@RequestBody UserPostDTO userPostDTO){
+      User userInput = DTOMapper.INSTANCE.convertUserPostDTOtoEntity(userPostDTO);
+
+      User userToBeLoggedIn = userService.loginUser(userInput);
+
+      return DTOMapper.INSTANCE.convertEntityToUserGetDTO(userToBeLoggedIn);
+  }
+
+    //for accessing specific user
+    @GetMapping ("/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public UserGetDTO getUser(@PathVariable Long userId) {
+        // fetch user
+        User foundUser = userService.getUser(userId);
+        //converting internal representation to api representation
+        return DTOMapper.INSTANCE.convertEntityToUserGetDTO(foundUser);}
+
+    // logout user
+    @PostMapping("/logout/{userId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseBody
+    public void logoutUser(@PathVariable Long userId){
+        userService.logoutUser(userId);
+
+    }
+
+    @PutMapping("/{userId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseBody
+    public void updateUser(@RequestBody UserPutDTO userPutDTO, @PathVariable Long userId){
+      if(!userPutDTO.getUserId().equals(userId)){
+          throw new ResponseStatusException(HttpStatus.NOT_FOUND, "user with the provided ID (" + userId + ") could not be found");
+      }
+      //userPutDTO.setUserId(userId);
+      User userInput = DTOMapper.INSTANCE.convertUserPutDTOtoEntity(userPutDTO);
+      userService.updateUser(userInput);
+    }
+}
