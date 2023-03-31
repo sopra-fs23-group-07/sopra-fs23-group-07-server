ffae516d2f2f27919a5e416613d9da0327ed68fe
Suleman <sulekhan111@gmail.com>
2023-03-31 15:14:26 +0200

    Implemented LobbyGetDTO, issue #144

--- src/main/java/ch/uzh/ifi/hase/soprafs23/rest/dto/LobbyGetDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/rest/dto/LobbyGetDTO.java
@@ -1,70 +1,96 @@
 package ch.uzh.ifi.hase.soprafs23.rest.dto;
 
+import ch.uzh.ifi.hase.soprafs23.constant.OverlapColor;
 import ch.uzh.ifi.hase.soprafs23.constant.UserStatus;
+import ch.uzh.ifi.hase.soprafs23.entity.EventDetails;
+import ch.uzh.ifi.hase.soprafs23.entity.Location;
+import ch.uzh.ifi.hase.soprafs23.entity.Member;
+import ch.uzh.ifi.hase.soprafs23.entity.User;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class LobbyGetDTO {
 
-  private Long userId;
-  private String email;
-  private String username;
-  private UserStatus status;
+  private Long lobbyId;
+
+  private String lobbyName;
+
+  private ArrayList<Member> lobbyMembers;
+
+  private Integer lobbyMaxMembers;
+
+  private String lobbyRegion;
+
+  private Integer lobbyTimeLimit;
+
   private String token;
-  private LocalDate creationDate;
 
-  private LocalDate birthdate;
+  private String lobbyDecidedLocation;
 
-  public Long getUserId() {
-    return userId;
-  }
+  private String lobbyDecidedSport;
 
-  public void setUserId(Long userId) {
-    this.userId = userId;
-  }
+  private Date lobbyDecidedDate;
 
-  public String getEmail() {
-    return email;
-  }
+  private ArrayList<Location> lobbyLocations;
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
 
-  public String getUsername() {
-    return username;
-  }
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
+  public void addLobbyMember(User user) {}
 
-  public UserStatus getStatus() {
-    return status;
-  }
+  public void removeLobbyMember(User user) {}
 
-  public void setStatus(UserStatus status) {
-    this.status = status;
-  }
+  public Long getLobbyId() {
+        return lobbyId;
+    }
+
+  public void setLobbyId(Long lobbyId) {
+        this.lobbyId = lobbyId;
+    }
+
+  public ArrayList<Member> getLobbyMembers() {return this.lobbyMembers; }
+
+  public Integer getLobbyMaxMembers() {return this.lobbyMaxMembers;}
+
+  public String getLobbyName() {
+        return lobbyName;
+    }
+
+  public void setLobbyName(String lobbyName) {
+        this.lobbyName = lobbyName;
+    }
+
+  public String getLobbyRegion() {
+        return lobbyRegion;
+    }
+
+  public void setLobbyRegion(String lobbyRegion) {
+        this.lobbyRegion = lobbyRegion;
+    }
+
+  public Integer getLobbyTimeLimit() {return this.lobbyTimeLimit; }
+
+  public String getLobbyDecidedSport() {return lobbyDecidedSport; }
+
+  public void setLobbyDecidedSport(String lobbyDecidedSport) {this.lobbyDecidedSport = lobbyDecidedSport;}
+
+  public Date getLobbyDecidedDate() {return lobbyDecidedDate;}
+
+  public void setLobbyDecidedDate(Date lobbyDecidedDate) {this.lobbyDecidedDate = lobbyDecidedDate;}
+
+  public String getLobbyDecidedLocation() {return lobbyDecidedLocation;}
+
+  public void setLobbyDecidedLocation(String lobbyDecidedLocation) {this.lobbyDecidedLocation = lobbyDecidedLocation;}
+
+  public Integer getNumberOfVotesForLocation(Location location) {return 0;}
 
   public String getToken() {
         return token;
     }
+
   public void setToken(String token) {
         this.token = token;
     }
-  public LocalDate getCreationDate() {
-        return creationDate;
-    }
-  public void setCreationDate(LocalDate creationDate) {
-        this.creationDate = creationDate;
-    }
 
-    public LocalDate getBirthdate(){
-      return birthdate;
-    }
-
-    public void setBirthdate(LocalDate birthdate){
-      this.birthdate = birthdate;
-    }
 }
