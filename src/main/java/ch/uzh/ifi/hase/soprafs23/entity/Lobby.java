3dfeefbe26ef5f86be6a7d86fc89fc72327cfc3d
Suleman <sulekhan111@gmail.com>
2023-04-03 15:03:48 +0200

    fixed issue with host member

--- src/main/java/ch/uzh/ifi/hase/soprafs23/entity/Lobby.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/entity/Lobby.java
@@ -35,6 +35,9 @@ public class Lobby implements Serializable {
   @Column(nullable = false, unique = true)
   private String lobbyName;
 
+  @Column(nullable = false, unique = true)
+  private Member hostMember;
+
   @Column(nullable = true)
   private ArrayList<Member> lobbyMembers;
 
@@ -62,6 +65,10 @@ public class Lobby implements Serializable {
   @Column(nullable = false, unique = true)
   private String token;
 
+
+   public Member getHostMember() {return hostMember; }
+
+  public void setHostMember(Member hostMember) {this.hostMember = hostMember; }
 
   public ArrayList<Location> getLobbyLocations() {return lobbyLocations; }
 
