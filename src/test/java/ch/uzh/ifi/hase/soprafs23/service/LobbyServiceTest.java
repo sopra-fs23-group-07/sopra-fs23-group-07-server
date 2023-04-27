f69bee2440aca9c07c9c1ab4b9df54fe4d470709
Suleman <sulekhan111@gmail.com>
2023-04-27 17:09:07 +0200

    test for Sprint 2

--- src/test/java/ch/uzh/ifi/hase/soprafs23/service/LobbyServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/service/LobbyServiceTest.java
@@ -21,6 +21,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.times;
 
+// test for Sprint2
 class LobbyServiceTest {
 
     @Mock
